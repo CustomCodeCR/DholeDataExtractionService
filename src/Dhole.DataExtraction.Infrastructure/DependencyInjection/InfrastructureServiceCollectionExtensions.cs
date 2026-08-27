68ea74813349fdb3eada62dccd22dbb72f045d28
Maurice Lang <106346768+MauLang18@users.noreply.github.com>
2026-08-27 07:37:03 -0600

    refactor(pdf): decorate extraction with filename carrier evidence

--- src/Dhole.DataExtraction.Infrastructure/DependencyInjection/InfrastructureServiceCollectionExtensions.cs
+++ src/Dhole.DataExtraction.Infrastructure/DependencyInjection/InfrastructureServiceCollectionExtensions.cs
@@ -101,7 +101,8 @@ public static class InfrastructureServiceCollectionExtensions
         services.AddScoped<ExcelDocumentExtractor>();
         services.AddScoped<IDocumentExtractor, LegacyCompatibleExcelDocumentExtractor>();
         services.AddScoped<IDocumentExtractor, CsvDocumentExtractor>();
-        services.AddScoped<IDocumentExtractor, PdfDocumentExtractor>();
+        services.AddScoped<PdfDocumentExtractor>();
+        services.AddScoped<IDocumentExtractor, CarrierAwarePdfDocumentExtractor>();
         services.AddScoped<IDocumentExtractor, EmailDocumentExtractor>();
         services.AddScoped<IDocumentExtractorFactory, DocumentExtractorFactory>();
 
